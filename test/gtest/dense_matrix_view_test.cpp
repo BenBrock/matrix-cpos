7a7a68114f9d1cd36730e1c8841055b94ce2a247
Benjamin Brock <brock@cs.berkeley.edu>
2023-10-07 04:32:43 +0000

    Update to include tests of CPOs for dense_matrix_view

--- test/gtest/dense_matrix_view_test.cpp
+++ test/gtest/dense_matrix_view_test.cpp
@@ -15,6 +15,7 @@ TEST(DenseMatrixView, RowColumnViews) {
 
     mc::dense_matrix_view view(v.begin(), {m, n});
 
+    // Iterate by row
     for (std::size_t i = 0; i < view.shape()[0]; i++) {
       auto row = view.row(i);
 
@@ -30,9 +31,36 @@ TEST(DenseMatrixView, RowColumnViews) {
       }
     }
 
+    for (auto&& [i, row] : mc::rows(view)) {
+      EXPECT_EQ(row.size(), view.shape()[1]);
+
+      std::size_t column_count = 0;
+      for (auto&& [j, v] : row) {
+        EXPECT_EQ(j, column_count);
+        T scalar_v = view[{i, j}];
+        EXPECT_EQ(v, scalar_v);
+
+        ++column_count;
+      }
+    }
+
+    // Iterate by column
     for (std::size_t j = 0; j < view.shape()[1]; j++) {
       auto column = view.column(j);
 
+      EXPECT_EQ(column.size(), view.shape()[0]);
+
+      std::size_t row_count = 0;
+      for (auto&& [i, v] : column) {
+        EXPECT_EQ(i, row_count);
+        T scalar_v = view[{i, j}];
+        EXPECT_EQ(v, scalar_v);
+
+        ++row_count;
+      }
+    }
+
+    for (auto&& [j, column] : mc::columns(view)) {
       EXPECT_EQ(column.size(), view.shape()[0]);
 
       std::size_t row_count = 0;
@@ -58,6 +86,7 @@ TEST(DenseMatrixView, DiagonalViews) {
 
     mc::dense_matrix_view view(v.begin(), {m, n});
 
+    // Iterate through "positive diagonals" (beginning along 0th row)
     for (std::ptrdiff_t d = 0; d < view.shape()[1]; d++) {
       // fmt::print("diagonal: {}\n", view.diagonal(d));
       auto diagonal = view.diagonal(d);
@@ -76,6 +105,7 @@ TEST(DenseMatrixView, DiagonalViews) {
       }
     }
 
+    // Iterate through "negative diagonals" (beginning along 0th column)
     for (std::ptrdiff_t d = -1; d > -std::ptrdiff_t(view.shape()[0]); d--) {
       auto diagonal = view.diagonal(d);
       std::size_t diagonal_size =
@@ -91,9 +121,41 @@ TEST(DenseMatrixView, DiagonalViews) {
       }
     }
 
+    // Iterate through all diagonals
     for (std::ptrdiff_t d = 0; d < view.num_diagonals(); d++) {
       auto diagonal = view.diagonal(d);
 
+      std::ptrdiff_t d_neg = d;
+
+      if (d >= view.shape()[1]) {
+        d_neg -= std::ptrdiff_t(view.num_diagonals());
+      }
+
+      std::size_t diagonal_size;
+      if (d_neg >= 0) {
+        diagonal_size = std::min(view.shape()[0], view.shape()[1] - d_neg);
+      } else {
+        diagonal_size = std::min(view.shape()[1], view.shape()[0] + d_neg);
+      }
+      EXPECT_EQ(diagonal.size(), diagonal_size);
+
+      for (std::size_t e = 0; e < diagonal_size; e++) {
+        std::size_t i, j;
+        if (d_neg >= 0) {
+          i = e;
+          j = e + d_neg;
+        } else {
+          i = e - d_neg;
+          j = e;
+        }
+        auto diagonal_value = view[{i, j}];
+        auto&& [d, diagonal_value_] = diagonal[e];
+        EXPECT_EQ(diagonal_value_, diagonal_value);
+        EXPECT_EQ(d, e);
+      }
+    }
+
+    for (auto&& [d, diagonal] : mc::diagonals(view)) {
       std::ptrdiff_t d_neg = d;
 
       if (d >= view.shape()[1]) {
